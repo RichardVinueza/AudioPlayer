ed90e379c60d22c5873f8795f31dd26a55b9dd59
Richard Vinueza <richardvinueza@gmail.com>
2020-04-16 12:03:12 +0100

    import media on home.page.ts and type it a variable on constructor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Media } from '@ionic-native/media/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public platform: Platform,
+    private media: Media
+  ) {}
 
 }
