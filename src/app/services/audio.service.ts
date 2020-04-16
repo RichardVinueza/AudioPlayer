0d837e178404811bee2605a180ab870e855fff62
Richard Vinueza <richardvinueza@gmail.com>
2020-04-16 12:22:32 +0100

    audio.service.ts changes and code

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+
+export const ipAddress = "http://192.168.0.16:8080";
+
+export class Artworks {
+  id: number;
+  name: string;
+  author: string;
+  country: string;
+  media: [MediaApi];
+}
+
+
+export class MediaApi {
+  id: number;
+  displayName: string;
+  fileName: string;
+  fileType: string;
+  extension: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class AudioService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getArtworksFromBackEnd() {
+    return this.http.get(ipAddress + "/artworks");
+  }
 }
