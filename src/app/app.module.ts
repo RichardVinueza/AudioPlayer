dedb881ab6943b06861db46cdcc47c904c94d0ef
Richard Vinueza <richardvinueza@gmail.com>
2020-04-16 12:14:50 +0100

    add module HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Media } from '@ionic-native/media/ngx';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -22,4 +28,4 @@ import { Media } from '@ionic-native/media/ngx';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
